719d3117cc9d122e6f0c9b1c81cd223a86da6329
Ayush Kumar <ayushsuperstar48@gmail.com>
2025-02-07 20:39:34 +0530

    linter bug fixed

--- src/app/api/projects/[projectId]/route.ts
+++ src/app/api/projects/[projectId]/route.ts
@@ -4,11 +4,8 @@ import { decrypt } from "@/db";
 import { db } from "@/db/index";
 import { projectsTable, tasksTable } from "@/db/schema";
 import { eq } from "drizzle-orm";
-
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { projectId: string } }
-) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function GET(request: NextRequest, params: any) {
   const session = (await cookies()).get("session")?.value;
   if (!session) {
     return NextResponse.json(
@@ -16,29 +13,34 @@ export async function GET(
       { status: 400 }
     );
   }
-  const projectId = Number((await params).projectId);
+
+  const projectId = Number(params.projectId);
   if (isNaN(projectId)) {
     return NextResponse.json(
       { error: "Invalid project ID", success: false },
       { status: 400 }
     );
   }
+
   try {
     const { username } = await decrypt(session);
     const projects = await db
       .select()
       .from(projectsTable)
       .where(eq(projectsTable.id, projectId));
-    if (projects[0].username !== username) {
+
+    if (!projects.length || projects[0].username !== username) {
       return NextResponse.json(
         { error: "User not authorized", success: false },
         { status: 403 }
       );
     }
+
     const tasks = await db
       .select()
       .from(tasksTable)
       .where(eq(tasksTable.projectId, projectId));
+
     return NextResponse.json({ tasks, project: projects[0] });
   } catch (error) {
     console.error("Error getting projects:", error);
